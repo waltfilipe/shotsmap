59e82707e2726ece10eec48ba4d6e1d44fdbc650
Walter Filipe Zanchet <walter.fbzanchet@gmail.com>
2026-03-09 16:01:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ shots_blocked = df_shots[df_shots["resultado"] == "Bloqueado"]
 
 pitch = VerticalPitch(
     half=True,
+    pitch_type="statsbomb", 
     pitch_color="grass",
     line_color="white"
 )
